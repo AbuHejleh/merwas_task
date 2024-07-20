f9f5e1f9296c4c1aee5ca9a426e53273240fa6ff
AbuHejleh <73398976+AbuHejleh@users.noreply.github.com>
2024-07-20 18:05:11 +0300

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,64 @@
+import AboutUs from "@/components/AboutUs/AboutUs";
+import ContactBox from "@/components/ContactUs/ContactBox";
+import Footer from "@/components/Footer/Footer";
+import DoubleText from "@/components/Home/DoubleText";
+import ImagesGrid from "@/components/Home/ImagesGrid";
+import VideoCard from "@/components/Home/VideoCard";
+import HomeImageWithText from "@/components/Navbar/HomeImageWithText";
+import PartnersGrid from "@/components/Partners/PartnersGrid";
+import Sections from "@/components/Sections";
+import ServicesCard from "@/components/Services/ServicesCard";
+import { servicesList } from "@/utils/constants";
 import React from "react";
-import Welcome from "./welcome/page";
 
 const Home = () => {
   return (
-    <>
-      <Welcome />
-    </>
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center">
+        <HomeImageWithText />
+        <DoubleText />
+        <div className="pb-[7rem]">
+          <ImagesGrid />
+        </div>
+
+        <VideoCard />
+        <Sections
+          subTitle="With lots of unique blocks, you can easily build a page easily without any coding."
+          title="About Us"
+          id="about"
+        />
+        <AboutUs />
+
+        <Sections
+          subTitle="With lots of unique blocks, you can easily build a page easily without any coding."
+          title="Services"
+          id="services"
+        />
+      </div>
+
+      {servicesList.map((items, index) => (
+        <ServicesCard {...items} key={index} />
+      ))}
+
+      <div className="bg-primaryColor w-full  flex flex-col">
+        <Sections
+          subTitle="With lots of unique blocks, you can easily build a page easily without any coding. "
+          title="Partners"
+          id="partners"
+          isColored={true}
+        />
+        <PartnersGrid />
+      </div>
+
+      <Sections
+        subTitle="With lots of unique blocks, you can easily build a page easily without any coding."
+        title="Contact Us"
+        id="contact"
+      />
+      <ContactBox />
+
+      <Footer />
+    </div>
   );
 };
 
--- src/components/ContactUs/CompanyLocation.tsx
+++ src/components/ContactUs/CompanyLocation.tsx
@@ -2,41 +2,35 @@
 // pages/map.tsx
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const CompanyLocation: React.FC = () => {
-  React.useEffect(() => {
-    // Update default icon options without the _getIconUrl fix
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl:
-        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
-      iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-      shadowUrl:
-        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-    });
-  }, []);
+  React.useEffect(() => {}, []);
 
-  return (
-    <div className="w-1/2 z-0">
-      <h1>My OpenStreetMap</h1>
-      <MapContainer
-        center={[40.7128, -74.006]}
-        zoom={13}
-        style={{ height: "500px", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[40.7128, -74.006]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+  if (typeof window !== "undefined") {
+    return (
+      <div className="w-1/2 z-0">
+        <h1>My OpenStreetMap</h1>
+        <MapContainer
+          center={[40.7128, -74.006]}
+          zoom={13}
+          style={{ height: "500px", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[40.7128, -74.006]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 export default CompanyLocation;
